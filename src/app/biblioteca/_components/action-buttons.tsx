0bd03269489fac5cfe7c10e00ecbb8d71efe9703
Alexander Armua <alexanderarmua1@gmail.com>
2024-07-31 22:05:41 -0300

    Small changes

--- src/app/biblioteca/_components/action-buttons.tsx
+++ src/app/biblioteca/_components/action-buttons.tsx
@@ -3,6 +3,8 @@ import { BibliotecaFilterMateria } from "./biblioteca-filter-materia";
 import { BibliotecaFilterText } from "./biblioteca-filter-text";
 import { BibliotecaNewLibro } from "./biblioteca-new-book";
 import { type inputGetBooks } from "@/shared/biblioteca-filter.schema";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 type BibliotecaFilters = z.infer<typeof inputGetBooks>;
 
@@ -13,8 +15,11 @@ type ActionButtonsProps = {
 export const ActionButtons = ({ filters }: ActionButtonsProps) => {
   return (
     <div className="relative flex w-full flex-col items-center justify-between space-y-2 md:flex-row-reverse  md:space-x-1.5">
-      <div className="relative flex w-full flex-row justify-end sm:basis-1/2 md:w-auto md:basis-1/3">
+      <div className="relative flex w-full flex-row justify-end space-x-2 sm:basis-1/2 md:w-auto md:basis-1/3">
         <BibliotecaNewLibro />
+        <Link href="/biblioteca/prestamos" passHref>
+          <Button color={"ghost"}>Ir a prestamos</Button>
+        </Link>
       </div>
 
       <div className="flex w-full flex-row space-x-3 md:basis-1/2">
