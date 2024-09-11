64fd118ad010588dc4cc276615bae72369227215
Alexander Armua <alexanderarmua1@gmail.com>
2024-09-11 18:19:14 -0300

    Agregado columna disponible en front

---
+++ prisma/migrations/20240911194127_agregado_disponible_libro_equipo/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Equipo" ADD COLUMN     "disponible" BOOLEAN NOT NULL DEFAULT true;
+
+-- AlterTable
+ALTER TABLE "Libro" ADD COLUMN     "disponible" BOOLEAN NOT NULL DEFAULT true;
