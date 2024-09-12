fc21b35f7be54b44cc8916bf6defafdece51c022
Alexander Armua <alexanderarmua1@gmail.com>
2024-09-12 17:10:22 -0300

    Mejorada arquitectura de la pantalla de detalle

---
+++ src/app/biblioteca/libros/_components/info-prestamos-libro.tsx
@@ -0,0 +1,51 @@
+import { Skeleton } from "@/components/ui/skeleton";
+import { api } from "@/trpc/react";
+
+export const LibroInformacionPrestamos = ({ libroId }: { libroId: number }) => {
+  //   const { data: libro, isLoading, isError } = api.biblioteca.libroPorId.useQuery({ libroId });
+
+  //   if (isError) {
+  //     return <div>Error al cargar...</div>;
+  //   }
+
+  return (
+    <>
+      <div className="my-2 flex w-full flex-col space-y-4 px-0 text-left text-2xl md:px-6">Historial de Prestamos:</div>
+      <div className="flex w-full flex-col space-y-4 px-12">
+        <table className="w-full table-auto border-collapse">
+          <thead>
+            <tr>
+              <th>Prestamo #</th>
+              <th>Prestado a</th>
+              <th>Fecha del prestamo</th>
+              <th>Fecha de finalización</th>
+              <th>Prestó</th>
+              <th>Renovado</th>
+              <th>Recibió</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>UTN</td>
+              <td>2023-01-01</td>
+              <td>2023-01-01</td>
+              <td>100</td>
+              <td>100</td>
+              <td>0</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>UTN</td>
+              <td>2023-01-01</td>
+              <td>2023-01-01</td>
+              <td>100</td>
+              <td>100</td>
+              <td>0</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
