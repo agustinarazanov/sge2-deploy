a01df81ea3966eb0adfdb599f4b420f9ef73d7f5
comejia <comejia@teco.com.ar>
2024-09-15 20:06:21 -0300

    Informacion basica de equipo

--- src/app/equipos/(listado)/estado-equipo.tsx
+++ src/app/equipos/(listado)/estado-equipo.tsx
@@ -1,18 +1,19 @@
 import { Badge } from "@/components/ui/badge";
+import PrestarEquipoModal from "../_components/modal-prestar";
 
 type EstadoEquipoProps = {
   id: number;
   disponible: boolean;
 };
 
-export default function EstadoEquipo({ disponible }: EstadoEquipoProps) {
+export default function EstadoEquipo({ disponible, id }: EstadoEquipoProps) {
   if (disponible) {
     return (
       <div className="flex flex-col">
         <Badge variant={"default"} color={"success"} className="w-full text-center">
           <div className="w-full">Disponible</div>
         </Badge>
-        {/* <PrestarLibroModal libroId={id} /> */}
+        {<PrestarEquipoModal equipoId={id} />}
       </div>
     );
   }
---
+++ src/app/equipos/_components/info-basica-equipo.tsx
@@ -0,0 +1,51 @@
+import { Skeleton } from "@/components/ui/skeleton";
+import { api } from "@/trpc/react";
+
+export const EquipoInformacionBasica = ({ equipoId }: { equipoId: number }) => {
+  const { data: equipo, isLoading, isError } = api.equipos.equipoPorId.useQuery({ id: equipoId });
+
+  if (isError) {
+    return <div>Error al cargar...</div>;
+  }
+
+  return (
+    <>
+      <div className="my-2 flex w-full flex-col space-y-4 px-0 text-left text-2xl md:px-6">Descripción:</div>
+      <div className="flex w-full flex-col space-y-4 px-12">
+        <div className="flex w-full flex-row lg:flex-row lg:justify-between lg:gap-x-4">
+          <div className="w-full">
+            <u>Numero de serie:</u> {isLoading ? <Skeleton className="h-4 w-full" /> : `${equipo?.numeroSerie}`}
+          </div>
+        </div>
+
+        <div className="flex w-full flex-row lg:flex-row lg:justify-between lg:gap-x-4">
+          <div className="w-full">
+            <u>Tipo:</u> {isLoading ? <Skeleton className="h-4 w-full" /> : `${equipo?.tipo.nombre}`}
+          </div>
+        </div>
+
+        <div className="flex w-full flex-row gap-x-4 lg:flex-row lg:justify-between">
+          <div className="basis-1/3">
+            <u>Inventario:</u> {isLoading ? <Skeleton className="h-4 w-full" /> : `${equipo?.inventarioId}`}
+          </div>
+          <div className="basis-1/3">
+            <u>Marca:</u> {isLoading ? <Skeleton className="h-4 w-full" /> : `${equipo?.marca.nombre}`}
+          </div>
+          <div className="basis-1/3">
+            <u>Modelo:</u> {isLoading ? <Skeleton className="h-4 w-full" /> : `${equipo?.modelo}`}
+          </div>
+        </div>
+
+        <div className="flex w-full flex-row gap-x-4 lg:flex-row lg:justify-between">
+          <div className="w-full">
+            <u>Palabras clave:</u> {isLoading ? <Skeleton className="h-4 w-full" /> : `${equipo?.palabrasClave}`}
+          </div>
+
+          <div className="w-full">
+            <u>Sede:</u> {isLoading ? <Skeleton className="h-4 w-full" /> : `${equipo?.sede.nombre}`}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
