88c88936d4e6e70537f4b9e7b8853e8c292ea293
comejia <comejia@teco.com.ar>
2024-09-15 20:06:21 -0300

    Informacion basica de equipo

---
+++ src/app/equipos/_components/modal-prestar.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { useState } from "react";
+
+import { Button } from "@/components/ui";
+
+import ModalDrawer from "@/app/_components/modal/modal-drawer";
+import { Separator } from "@radix-ui/react-separator";
+import { EquipoInformacionBasica } from "./info-basica-equipo";
+
+type PrestarEquipoModalProps = {
+  equipoId: number;
+};
+
+export default function PrestarEquipoModal({ equipoId }: PrestarEquipoModalProps) {
+  const [open, setOpen] = useState(false);
+
+  const handleSubmit = () => setOpen(false);
+
+  const handleCancel = () => setOpen(false);
+
+  return (
+    <ModalDrawer
+      trigger={
+        <Button
+          title="Prestar"
+          variant="default"
+          color="secondary"
+          size="sm"
+          className="mt-2 w-full rounded-full border-none"
+        >
+          Prestar
+        </Button>
+      }
+      titulo={`Prestar Equipo`}
+      open={open}
+      onOpenChange={setOpen}
+    >
+      <div className="flex flex-col">
+        <EquipoInformacionBasica equipoId={equipoId} />
+
+        <Separator className="my-8 border-2" />
+
+        {/*<LibroFormPrestarORenovar equipoId={equipoId} onCancel={handleCancel} onSubmit={handleSubmit} />*/}
+      </div>
+    </ModalDrawer>
+  );
+}
