3ef7ef1e317299b62e466c0f6762739127f69b14
Alexander Armua <alexanderarmua1@gmail.com>
2024-09-15 16:31:01 -0300

    Implementado filtro dinamico en Laboratorios Cerrado

--- src/shared/filters/cursos-filter.schema.ts
+++ src/shared/filters/cursos-filter.schema.ts
@@ -28,9 +28,9 @@ export const inputGetCursos = z.object({
     .refine((value) => parseInt(value) >= 0, { message: "Debe ser mayor o igual a 0" })
     .catch("0"),
   orderBy: z
-    .enum(["anioDeCarrera", "materia_nombre", "sede_nombre", "ac", "turno", "division_nombre"])
-    .default("anioDeCarrera")
-    .catch("anioDeCarrera"),
+    .enum(["division_nombre", "ac", "turno", "sede_nombre"])
+    .default("division_nombre")
+    .catch("division_nombre"),
   orderDirection: z.enum(["asc", "desc"]).default("asc").catch("asc"),
   searchText: z.string().default(""),
   materia: z
