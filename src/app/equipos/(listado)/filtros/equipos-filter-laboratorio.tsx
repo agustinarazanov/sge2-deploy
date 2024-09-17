9be47ed72ba0823444a71f0e1edf0eb2f08d64e5
comejia <comejia@teco.com.ar>
2024-09-16 21:09:21 -0300

    Fix filtro de laboratorio

--- src/app/equipos/(listado)/filtros/equipos-filter-laboratorio.tsx
+++ src/app/equipos/(listado)/filtros/equipos-filter-laboratorio.tsx
@@ -1,20 +1,13 @@
 "use client";
 
-import React from "react";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui";
+import React, { useMemo, useState } from "react";
+import { Autocomplete, Select, SelectTrigger, SelectValue } from "@/components/ui";
 import { api } from "@/trpc/react";
 import { Skeleton } from "@/components/ui/skeleton";
 import { type z } from "zod";
 import { useEquiposQueryParam } from "../../_hooks/use-equipos-query-param";
 import { type inputGetEquipos } from "@/shared/filters/equipos-filter.schema";
+import { estaDentroDe } from "@/shared/string-compare";
 
 type EquiposFilters = z.infer<typeof inputGetEquipos>;
 
@@ -25,9 +18,30 @@ type Props = {
 export const EquiposFilterLaboratorio = ({ filters }: Props) => {
   const { laboratorio, onLaboratorioChange } = useEquiposQueryParam(filters);
 
-  const { data, isLoading, isError } = api.admin.laboratorios.getAll.useQuery({ sedeId: "4" });
+  const [query, setQuery] = useState("");
 
-  const laboratorios = data?.laboratorios;
+  const { data, isLoading, isError } = api.admin.laboratorios.getAll.useQuery({ sedeId: undefined });
+
+  const laboratorios = useMemo(() => {
+    if (!data) return [];
+
+    return data.laboratorios
+      .map((item) => {
+        const { id, nombre, sede } = item;
+        return {
+          id: id,
+          label: `${nombre} (${sede.nombre})`,
+          data: item,
+        };
+      })
+      .filter((item) => !query || estaDentroDe(query, item.label));
+  }, [data, query]);
+
+  const currentLaboratorio = useMemo(() => {
+    if (!laboratorios) return null;
+
+    return laboratorios.find((item) => String(item.id) === laboratorio);
+  }, [laboratorios, laboratorio]);
 
   if (isLoading) {
     return (
@@ -59,26 +73,23 @@ export const EquiposFilterLaboratorio = ({ filters }: Props) => {
 
   return (
     <div className="w-full">
-      <Select onValueChange={(value) => onLaboratorioChange(value)} value={laboratorio}>
-        <div className="flex flex-row items-center space-x-2">
-          <SelectGroup className="w-full">
-            <SelectLabel className="sr-only">Selecciona un laboratorio</SelectLabel>
-            <SelectTrigger
-              id="selectLaboratorio"
-              className="h-10 transition-colors focus:border-primary focus:ring-0 group-hover:border-input-hover"
-            >
-              <SelectValue placeholder="Selecciona un laboratorio" />
-            </SelectTrigger>
-            <SelectContent>
-              {(laboratorios ?? []).map((option) => (
-                <SelectItem key={option.id} value={String(option.id)}>
-                  {option.nombre} ({option.esAbierto ? "Abierto" : "Cerrado"})
-                </SelectItem>
-              ))}
-            </SelectContent>
-          </SelectGroup>
-        </div>
-      </Select>
+      <Autocomplete
+        async
+        items={laboratorios}
+        noOptionsComponent={
+          <div className="flex flex-col items-center justify-center gap-2 px-4 py-6 text-sm text-white">
+            <span>No se encontró el laboratorio</span>
+          </div>
+        }
+        className={""}
+        onQueryChange={setQuery}
+        isLoading={isLoading}
+        placeholder="Buscar por laboratorio"
+        clearable
+        debounceTime={0}
+        value={currentLaboratorio}
+        onChange={(value) => onLaboratorioChange(value?.id ? String(value.id) : "")}
+      />
     </div>
   );
 };
