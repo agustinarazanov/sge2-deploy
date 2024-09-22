75f0f1686d103c596a366b3a5b7fda3a582b669e
Alexander Armua <alexanderarmua1@gmail.com>
2024-09-22 02:27:33 -0300

    Agregada metadata basica

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,37 @@ export const metadata = {
   title: "SGE",
   description: "Creado por el equipo de SGE con ❤️",
   icons: "/electrical-circuit.png",
+  openGraph: {
+    type: "website",
+    locale: "es_ES",
+    url: "https://sge-2.vercel.app/",
+    countryName: "Argentina",
+    title: "SGE 2.0",
+    siteName: "SGE 2.0",
+    description: "Creado por el equipo de SGE con ❤️",
+    images: [
+      {
+        url: "https://sge-2.vercel.app/electrical-circuit.png",
+        width: 800,
+        height: 800,
+        alt: "SGE 2.0 Logo",
+        type: "image/png",
+      },
+    ],
+  },
+  twitter: {
+    title: "SGE 2.0",
+    description: "Creado por el equipo de SGE con ❤️",
+    images: [
+      {
+        url: "https://sge-2.vercel.app/electrical-circuit.png",
+        width: 800,
+        height: 800,
+        alt: "SGE 2.0 Logo",
+        type: "image/png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
