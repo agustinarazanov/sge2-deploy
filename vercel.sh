54620fd228d50662482f1410b85efaa930e020d8
Agustina Razanov <agustinarazanov@gmail.com>
2024-09-22 16:25:22 -0300

    update populate with pg_dump

--- vercel.sh
+++ vercel.sh
@@ -1,5 +1,5 @@
-npx prisma db execute --file prisma/drop.sql
-npx prisma generate
-npx prisma db push
+# npx prisma db execute --file prisma/drop.sql
+# npx prisma generate
+# npx prisma db push
 npx prisma db execute --file prisma/populate.sql
 npm run build
