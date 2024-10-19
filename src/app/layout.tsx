e9c1e0344984e0b6b45340b97a34541c8f29d27a
Alexander Armua <alexanderarmua1@gmail.com>
2024-10-19 11:35:30 -0300

    Navbar siempre top

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,43 +5,9 @@ import { GeistSans } from "geist/font/sans";
 import { TRPCReactProvider } from "@/trpc/react";
 import Navbar from "./_components/navbar/navbar";
 import { Toaster } from "@/components/ui";
+import { metadataLayoutPrincipal } from "./metadata";
 
-export const metadata = {
-  title: "SGE",
-  description: "Creado por el equipo de SGE con ❤️",
-  icons: "/electrical-circuit.png",
-  openGraph: {
-    type: "website",
-    locale: "es_ES",
-    url: "https://sge-2.vercel.app/",
-    countryName: "Argentina",
-    title: "SGE 2.0",
-    siteName: "SGE 2.0",
-    description: "Creado por el equipo de SGE con ❤️",
-    images: [
-      {
-        url: "https://sge-2.vercel.app/electrical-circuit.png",
-        width: 800,
-        height: 800,
-        alt: "SGE 2.0 Logo",
-        type: "image/png",
-      },
-    ],
-  },
-  twitter: {
-    title: "SGE 2.0",
-    description: "Creado por el equipo de SGE con ❤️",
-    images: [
-      {
-        url: "https://sge-2.vercel.app/electrical-circuit.png",
-        width: 800,
-        height: 800,
-        alt: "SGE 2.0 Logo",
-        type: "image/png",
-      },
-    ],
-  },
-};
+export const metadata = metadataLayoutPrincipal;
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -57,7 +23,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <link rel="apple-touch-icon" type="image/png" sizes="144x144" href="/apple-touch-icon-ipad-retina-152x152.png" />
       <body>
         <TRPCReactProvider>
-          <header>
+          <header className="sticky top-0 z-40 bg-white">
             <Navbar />
           </header>
           <Toaster position="top-right" duration={3000} />
---
+++ src/app/metadata.ts
@@ -0,0 +1,36 @@
+export const metadataLayoutPrincipal = {
+  title: "SGE",
+  description: "Sistema de Gestión Electrónica",
+  icons: "/electrical-circuit.png",
+  openGraph: {
+    type: "website",
+    locale: "es_ES",
+    url: "https://sge-2.vercel.app/",
+    countryName: "Argentina",
+    title: "SGE 2.0",
+    siteName: "SGE 2.0",
+    description: "Sistema de Gestión Electrónica",
+    images: [
+      {
+        url: "https://sge-2.vercel.app/electrical-circuit.png",
+        width: 800,
+        height: 800,
+        alt: "SGE 2.0 Logo",
+        type: "image/png",
+      },
+    ],
+  },
+  twitter: {
+    title: "SGE 2.0",
+    description: "Sistema de Gestión Electrónica",
+    images: [
+      {
+        url: "https://sge-2.vercel.app/electrical-circuit.png",
+        width: 800,
+        height: 800,
+        alt: "SGE 2.0 Logo",
+        type: "image/png",
+      },
+    ],
+  },
+};
